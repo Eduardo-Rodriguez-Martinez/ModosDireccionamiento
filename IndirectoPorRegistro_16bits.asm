2858ce72fafbb1b423f9082ce755e0c8057cbcc9
Eduardo Rodriguez-Martinez <87337432+Eduardo-Rodriguez-Martinez@users.noreply.github.com>
2024-08-20 11:52:45 -0600

    Fixed base-register

--- IndirectoPorRegistro_16bits.asm
+++ IndirectoPorRegistro_16bits.asm
@@ -9,16 +9,16 @@ main proc
     mov ax, @data       ; Mueve la dirección de la sección de datos al registro ax
     mov ds, ax          ; Mueve la dirección de la sección de datos al registro ds
 
-    mov ax, OFFSET tabla; Carga el desplazamiento de la tabla en ax
-    mov bx, [ax]        ; Carga el primer elemento de la tabla en bx (?)
-    mov ch, [ax]
+    mov bx, OFFSET tabla; Carga el desplazamiento de la tabla en bx
+    mov ax, [bx]        ; Carga el primer elemento de la tabla en ax (?)
+    mov ch, [bx]
 
     mov dx, -1          ; Carga 0FFFFh en el registro dx
-    mov [ax], dl
-    mov [ax], dx
+    mov [bx], dl
+    mov [bx], dx
 
-    mov BYTE PTR [ax], 0A5h      ; Carga 0A5h en la dirección de memoria apuntada por ax
-    mov WORD PTR [ax], 1F7Bh     ; Carga 1F7Bh en la dirección de memoria aputnada por ax
+    mov BYTE PTR [bx], 0A5h      ; Carga 0A5h en la dirección de memoria apuntada por bx
+    mov WORD PTR [bx], 1F7Bh     ; Carga 1F7Bh en la dirección de memoria aputnada por bx
 
     mov ax, 4c00h
     int 21h
